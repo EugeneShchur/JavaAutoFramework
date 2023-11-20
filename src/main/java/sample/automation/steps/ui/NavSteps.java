cab47dcc894cf5448d914055d755285370f8bc34
EugeneShchur <eugene.shchur@nixsolutions.com>
2023-11-20 15:49:46 +0200

    Add saveScreenshot for navigate method to debug GitHub runner issue

--- src/main/java/sample/automation/steps/ui/NavSteps.java
+++ src/main/java/sample/automation/steps/ui/NavSteps.java
@@ -1,11 +1,13 @@
 package sample.automation.steps.ui;
 
+import static sample.automation.utils.DateHelper.formatTimestampIntoFilenameDate;
 import static sample.automation.utils.StringHelper.EMPTY_STRING;
 
 import org.openqa.selenium.WebDriver;
 
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
+import sample.automation.utils.AttachmentsHelper;
 
 @Log4j2
 public abstract class NavSteps extends CommonPageSteps {
@@ -34,6 +36,8 @@ public abstract class NavSteps extends CommonPageSteps {
       return steps;
     } catch (Exception e) {
       log.error(e);
+      AttachmentsHelper.saveScreenshotPNG(driver, "navigate_failed_"
+          + formatTimestampIntoFilenameDate(System.currentTimeMillis()));
       throw new AssertionError("Failed to navigate on the target page!");
     }
   }
